e94ba9407fa9fcbc6eee60a23ef731c241fcbb3b
Nicholas <nenglish443@outlook.com>
2018-06-07 01:29:21 -0400

    Update Database.php

--- src/Database.php
+++ src/Database.php
@@ -1,1 +1,24 @@
+<?php
+declare(strict_types=1);
+/**
+ * Viper Content Management System.
+ *
+ * @author Nicholas English   <https://github.com/Auvipev>.
+ * @author Viper Contributors <https://github.com/Auvipev/Viper/graphs/contributors>.
+ *
+ * @package Viper.
+ */
 
+namespace Auvipev\Viper;
+
+/**
+ * The database class.
+ *
+ * @class      Database.
+ * @extends    Engine.
+ * @implements DatabaseInterface.
+ * @implements InjectableObject.
+ */
+class Database extends Engine implements DatabaseInterface, InjectableObject
+{
+}
