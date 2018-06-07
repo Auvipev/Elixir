2c5985fc43d357e666cfac6058321b8feced660d
Nicholas <nenglish443@outlook.com>
2018-06-07 01:19:16 -0400

    Create InjectableObject.php

---
+++ src/InjectableObject.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * Viper Content Management System.
+ *
+ * @author Nicholas English   <https://github.com/Auvipev>.
+ * @author Viper Contributors <https://github.com/Auvipev/Viper/graphs/contributors>.
+ *
+ * @package Viper.
+ */
+
+namespace Auvipev\Viper;
+
+/**
+ * @interface InjectableObject.
+ */
+interface InjectableObject
+{
+}
