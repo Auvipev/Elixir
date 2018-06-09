8edc874b6f7e1b8fc4d34ca055faf89ed3545443
Nicholas <nenglish443@outlook.com>
2018-06-09 03:44:49 -0400

    Add bootstrap autoloading.

---
+++ bin/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+/**
+ * Elixir: A guide to writing clean, secure, and reusable PHP code.
+ *
+ * @author Nicholas English    <https://github.com/Auvipev>.
+ * @author Elixir Contributors <https://github.com/Auvipev/Elixir/graphs/contributors>.
+ *
+ * @package Elixir.
+ */
