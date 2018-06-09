8eb02943e77c23b14f05197f384dec19573f5951
Nicholas <nenglish443@outlook.com>
2018-06-09 03:43:52 -0400

    Add version declaration.

---
+++ bin/version.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+/**
+ * Elixir: A guide to writing clean, secure, and reusable PHP code.
+ *
+ * @author Nicholas English    <https://github.com/Auvipev>.
+ * @author Elixir Contributors <https://github.com/Auvipev/Elixir/graphs/contributors>.
+ *
+ * @package Elixir.
+ */
+
+define('ELIXIR_VERSION_ID', 100000000);
+define('ELIXIR_VERSION', 'v1.0.0');
