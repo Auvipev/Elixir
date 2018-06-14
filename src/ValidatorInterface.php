4e75110cb66d40977677e47a65471f0c57a01a35
Nicholas <nenglish443@outlook.com>
2018-06-13 23:23:41 -0400

    Add validator interface.

--- src/ValidatorInterface.php
+++ src/ValidatorInterface.php
@@ -1,1 +1,35 @@
+<?php
+declare(strict_types=1);
+/**
+ * Viper Content Management System.
+ *
+ * @author Nicholas English   <https://github.com/Auvipev>.
+ * @author Viper Contributors <https://github.com/Auvipev/Viper/graphs/contributors>.
+ *
+ * @package Viper.
+ */
 
+namespace Auvipev\Viper;
+
+/**
+ * The validator interface.
+ *
+ * @link <https://secure.php.net/manual/en/class.unexpectedvalueexception.php>.
+ *
+ * @interface ValidatorInterface.
+ */
+interface ValidatorInterface
+{
+
+    /**
+     * Check to see if the test case is valid against the requested validation object based on
+     * any options passed.
+     *
+     * @param string $validationObject The validation object.
+     * @param mixed  $testCase         The test case (Can be anything).
+     * @param array  $options          Any options to pass.
+     *
+     * @return bool Returns TRUE if th test case passes and FALSE if it does not.
+     */
+    public function isValid(string $validationObject, $testCase = null, array $options = array()): bool;
+}
