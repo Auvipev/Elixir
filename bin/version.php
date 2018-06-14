6c8dd693a01c6c19092af18aae959a4947bbdeea
Nicholas <nenglish443@outlook.com>
2018-06-13 22:11:41 -0400

    Add version declaration.

--- bin/version.php
+++ bin/version.php
@@ -1,1 +1,13 @@
+<?php
+declare(strict_types=1);
+/**
+ * Viper Content Management System.
+ *
+ * @author Nicholas English   <https://github.com/Auvipev>.
+ * @author Viper Contributors <https://github.com/Auvipev/Viper/graphs/contributors>.
+ *
+ * @package Viper.
+ */
 
+define('VIPER_ENGINE_VERSION_ID', 100000000);
+define('VIPER_ENGINE_VERSION', 'v1.0.0');
