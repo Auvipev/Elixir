8cb53eda06436c25a9e806adee71d6a09fd83ca7
Nicholas <nenglish443@outlook.com>
2018-06-14 22:27:27 -0400

    Add validation object interface.

--- src/Validation/ValidatorObjectInterface.php
+++ src/Validation/ValidatorObjectInterface.php
@@ -1,1 +1,21 @@
+<?php
+declare(strict_types=1);
+/**
+ * Viper Content Management System.
+ *
+ * @author Nicholas English   <https://github.com/Auvipev>.
+ * @author Viper Contributors <https://github.com/Auvipev/Viper/graphs/contributors>.
+ *
+ * @package Viper.
+ */
 
+namespace Auvipev\Viper;
+
+/**
+ * The validation object interface.
+ *
+ * @interface ValidatorObjectInterface.
+ */
+interface ValidatorObjectInterface
+{
+}
