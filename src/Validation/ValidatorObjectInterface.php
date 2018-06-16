abd760c40afe58bb6c2d106516cde320ed719f62
Nicholas <nenglish443@outlook.com>
2018-06-16 17:23:26 -0400

    Update name.

--- src/Validation/ValidatorObjectInterface.php
+++ src/Validation/ValidatorObjectInterface.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 /**
- * Viper Content Management System.
+ * Viper Engine Management System.
  *
  * @author Nicholas English   <https://github.com/Auvipev>.
  * @author Viper Contributors <https://github.com/Auvipev/Viper/graphs/contributors>.
