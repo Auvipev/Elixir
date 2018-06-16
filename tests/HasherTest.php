686f920de4965d850c871a3ce1dfc5ae55eae98c
Nicholas <nenglish443@outlook.com>
2018-06-16 16:39:45 -0400

    Add some more tests.

--- tests/HasherTest.php
+++ tests/HasherTest.php
@@ -15,6 +15,8 @@ use PHPUnit\Framework\TestCase;
 
 use function md5;
 
+use const PASSWORD_BCRYPT;
+
 /**
  * The hasher test class.
  *
@@ -23,13 +25,32 @@ use function md5;
 class HasherTest extends TestCase
 {
 
-    public function testHashVerify()
+    public function testHasher()
     {
-        $hasher = new Hasher(array());
+        $hasher = new Hasher(array(
+            'password_hash' => array(
+                'algo' => PASSWORD_BCRYPT,
+                'options' => array(
+                    'cost' => 11
+                )
+            )
+        ));
+        $generatedHashA = $hasher->hashPassword('Hello World!');
+        $generatedHashB = $hasher->hashPassword('Hello World!');
+        $generatedHashC = $hasher->hashPassword('Hello Tom!');
         $testHashA = md5('Hello World!');
         $testHashB = md5('Hello World!');
         $testHashC = md5('Hello Tom!');
         $this->assertTrue($hasher->verify($testHashA, $testHashB));
         $this->assertTrue(!$hasher->verify($testHashB, $testHashC));
+        $this->assertTrue($hasher->verifyPassword($generatedHashA, $generatedHashB));
+        $this->assertTrue(!$hasher->verifyPassword($generatedHashB, $generatedHashC));
+        $this->assertEquals($hasher->getPasswordHashInfo($generatedHashA), array(
+            'algo' => PASSWORD_BCRYPT,
+            'algoName' => 'bcrypt',
+            'options' => array(
+                'cost' => 11
+            )
+        ));
     }
 }
