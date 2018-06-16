db29ca3987ed24ba684afbf728caa808a0f15783
Nicholas <nenglish443@outlook.com>
2018-06-16 16:18:20 -0400

    Update links.

--- src/InjectableObject.php
+++ src/InjectableObject.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
  * Viper Content Management System.
  *
  * @author Nicholas English   <https://github.com/Auvipev>.
- * @author Viper Contributors <https://github.com/Auvipev/Viper/graphs/contributors>.
+ * @author Viper Contributors <https://github.com/Auvipev/Viper-Engine/graphs/contributors>.
  *
  * @package Viper.
  */
