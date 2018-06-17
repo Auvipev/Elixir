eeb583e4b3e1c455ec24412cc870cf65c22be73a
Nicholas <nenglish443@outlook.com>
2018-06-16 21:01:35 -0400

    Update name.

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -2,7 +2,7 @@
 <?php
 declare(strict_types=1);
 /**
- * Viper Content Management System.
+ * Viper Engine Management System.
  *
  * @author Nicholas English   <https://github.com/Auvipev>.
  * @author Viper Contributors <https://github.com/Auvipev/Viper-Engine/graphs/contributors>.
