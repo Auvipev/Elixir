9a73ba8ff8383d136539174be056c625cdaff0bd
Nicholas <nenglish443@outlook.com>
2018-06-16 21:00:28 -0400

    Add the engine function.

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -1,1 +1,15 @@
 
+<?php
+declare(strict_types=1);
+/**
+ * Viper Content Management System.
+ *
+ * @author Nicholas English   <https://github.com/Auvipev>.
+ * @author Viper Contributors <https://github.com/Auvipev/Viper-Engine/graphs/contributors>.
+ *
+ * @package Viper.
+ */
+
+function engine()
+{
+}
