c74aad5e9e5066fbcc86de2bf8cdb6e9d01eabfe
Nicholas <nenglish443@outlook.com>
2018-06-21 16:45:39 -0400

    Test db connection.

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -1,1 +1,39 @@
+<?php
+declare(strict_types=1);
+/**
+ * Viper Content Management System.
+ *
+ * @author Nicholas English   <https://github.com/Auvipev>.
+ * @author Viper Contributors <https://github.com/Auvipev/Viper-Engine/graphs/contributors>.
+ *
+ * @package Viper.
+ */
+namespace Auvipev\Viper;
 
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+
+/**
+ * The database test class.
+ *
+ * @class DatabaseTest.
+ */
+class DatabaseTest extends TestCase
+{
+    use TestCaseTrait;
+
+    /**
+     * @return PHPUnit\DbUnit\Database\Connection
+     */
+    public function getConnection()
+    {
+        $link = new Database([
+            'database' => [
+                'dns'      => 'mysql:host=localhost;dbname=testdb',
+                'username' => 'root',
+                'password' => ''
+            ]
+        ]);
+        return $this->createDefaultDBConnection($pdo, ':memory:');
+    }
+}
